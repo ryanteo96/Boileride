49232ef45821709dc8f86f88c763d9142f438b77
Ryan <ryanteo1996@gmail.com>
2018-09-13 17:11:26 -0400

    fix: default routing

--- web/index.js
+++ web/index.js
@@ -3,8 +3,8 @@ var path = require("path");
 
 var app = express();
 
-app.get("/signIn", function(req, res) {
-	res.sendFile(path.join(__dirname, "publicsignIn.html"));
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "public/signIn.html"));
 });
 
 app.listen(4000);
