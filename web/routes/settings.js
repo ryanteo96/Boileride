82ce527334c2a8485dcd79fe6946953d7ab5c1eb
Ryan <ryanteo1996@gmail.com>
2018-09-20 00:38:46 -0400

    feat: settings server connection

--- web/routes/settings.js
+++ web/routes/settings.js
@@ -6,4 +6,96 @@ router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/settings.html"));
 });
 
+router.post("/changeNickname", function(req, res) {
+	var data = {
+		nickname: req.body.nickname,
+	};
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/user/update",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+});
+
+router.post("/changeEmail", function(req, res) {
+	var data = {
+		email: req.body.email,
+	};
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/user/update",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+});
+
+router.post("/changePassword", function(req, res) {
+	var data = {
+		password: req.body.password,
+	};
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/user/update",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+});
+
+router.post("/changePhone", function(req, res) {
+	var data = {
+		phone: req.body.phone,
+	};
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/user/update",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+});
+
 module.exports = router;
