3e9ba4d0a6cd9a9636bbc17e1cc8ba4aa388b19b
Ryan <ryanteo1996@gmail.com>
2018-09-21 16:11:10 -0400

    refactor: signin server responses

---
+++ web/public/javascript/signIn.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	$("#signInForm").submit(function(data) {
+		data.preventDefault();
+
+		$.post(
+			"/signIn",
+			{
+				email: $("#emailSignIn").val(),
+				password: $("#passwordSignIn").val(),
+			},
+			function(res) {
+				console.log(res);
+
+				switch (res) {
+					case "0": {
+						window.location.href = "/home";
+
+						localStorage.key = "credentials";
+						localStorage.setItem(
+							"credentials",
+							JSON.stringify({
+								email: $("#emailSignIn").val(),
+								password: $("#passwordSignIn").val(),
+							}),
+						);
+						break;
+					}
+					case "1": {
+						alert("User does not exist.");
+						break;
+					}
+					case "2": {
+						alert("Invalid password.");
+						break;
+					}
+				}
+			},
+		);
+	});
+});
--- web/routes/signIn.js
+++ web/routes/signIn.js
@@ -2,36 +2,43 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const request = require("request");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto-js/sha3");
+const asyncHandler = require("express-async-handler");
+
 const router = express.Router();
 
 router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/signIn.html"));
 });
 
-router.post("/", function(req, res) {
-	var data = {
-		email: req.body.email,
-		password: req.body.password,
-	};
+router.post(
+	"/",
+	asyncHandler(async (req, res, next) => {
+		var data = {
+			email: req.body.email,
+			password: crypto(req.body.email + req.body.password).toString(),
+		};
 
-	console.log(data);
+		res.send("0");
+		console.log(data);
 
-	// temp server connection test
-	// var options = {
-	// 	uri: "http://localhost:8080/user/login",
-	// 	json: data,
-	// 	method: "POST",
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// };
+		// temp server connection test
+		var options = {
+			uri: "http://localhost:8080/user/login",
+			json: data,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-	// request(options, function(error, response) {
-	// 	console.log(error, response);
-	// 	return;
-	// });
-
-	res.redirect("/home");
-});
+		request(options, function(error, response) {
+			console.log(error, response);
+			res.send(response.result);
+			return;
+		});
+	}),
+);
 
 module.exports = router;
