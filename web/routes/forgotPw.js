076442f4d5581caa1d0f0cd3dd5a30b503a0ae63
LCLY <lchoo1996@gmail.com>
2018-09-21 00:21:52 -0400

    fix: change <button> element to <a> element under form

--- web/routes/forgotPw.js
+++ web/routes/forgotPw.js
@@ -6,6 +6,29 @@ router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/forgotPw.html"));
 });
 
+router.post("/", function(req, res) {
+	var data = {
+		email: req.body.email,		
+	};
 
+	console.log(data);
+
+	// temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/user/forgotpassword",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+
+	res.redirect("/resetPw");
+});
 
 module.exports = router;
