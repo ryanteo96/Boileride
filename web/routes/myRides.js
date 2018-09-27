cf0dc90c726cf17135d9dde4aba28eba10e1f0d8
LCLY <lchoo1996@gmail.com>
2018-09-27 19:07:46 -0400

    feat: added myOffer and myRequest related files

--- web/routes/myRides.js
+++ web/routes/myRides.js
@@ -6,4 +6,12 @@ router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/myRides.html"));
 });
 
+router.get("/myRequest", function(req,res){
+	res.sendFile(path.join(__dirname, "../public/html/myRequest.html"));
+});
+
+router.get("/myOffer", function(req,res){
+	res.sendFile(path.join(__dirname, "../public/html/myOffer.html"));
+});
+
 module.exports = router;
