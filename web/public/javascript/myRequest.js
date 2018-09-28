fbf1b5b75fadd443c2e0d84acc783789c90cf486
LCLY <lchoo1996@gmail.com>
2018-09-28 00:14:29 -0400

    feat: generate list for myRequest

--- web/public/javascript/myRequest.js
+++ web/public/javascript/myRequest.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
 	$("#myRequest").click(function(data) {
 		data.preventDefault();
 
-		var credentials = localStorage.getItem("credentials");
-		var obj = JSON.parse(credentials);
+		// var credentials = localStorage.getItem("credentials");
+		// var obj = JSON.parse(credentials);
 
-		console.log(obj.userid);
+		// console.log(obj.userid);
 
 		$.post(
 			"/myRides/myRequest",
 			{
-				userid: obj.userid,
+				// userid: obj.userid,
+				userid:"temp"
 			},
 			function(res) {
 				switch (res.result) {
@@ -23,7 +24,7 @@ $(document).ready(function() {
 								requestlist: res.requestlist
 							}),
 						);		
-						window.location.href = "/myRequest";			
+						generateList();			
 						break;
 					}
 					case 1: {
@@ -39,3 +40,30 @@ $(document).ready(function() {
 		);
 	});
 });
+
+function generateList(){
+	var options = {
+		valueNames: [ 'name', 'born' ],
+		item: '<li><h3 class="name"></h3><p class="born"></p></li>'
+	};
+	
+	var values = [{
+		name: 'Jonny Strömberg',
+		born: 1986
+	},
+	{
+		name: 'Jonas Arnklint',
+		born: 1985
+	},
+	{
+		name: 'Martina Elm',
+		born: 1986
+	}];
+	
+	var userList = new List('users', options, values);
+	
+	userList.add({
+	name: "Gustaf Lindqvist",
+	born: 1983
+	});
+}
