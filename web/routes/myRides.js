d6eda714e26e8a92d142ee7ce0e75955626aa563
LCLY <lchoo1996@gmail.com>
2018-09-27 22:33:01 -0400

    refactor: moved myOffer and myRequest into myRides

--- web/routes/myRides.js
+++ web/routes/myRides.js
@@ -8,10 +8,50 @@ router.get("/", function(req, res) {
 
 router.get("/myRequest", function(req,res){
 	res.sendFile(path.join(__dirname, "../public/html/myRequest.html"));
+	var data = {
+		userid: req.body.userid,
+	};
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/ride/view/request",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
+	
 });
 
 router.get("/myOffer", function(req,res){
 	res.sendFile(path.join(__dirname, "../public/html/myOffer.html"));
+	var data = {
+		userid: req.body.userid,
+	};
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/ride/view/offer",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	return;
+	// });
 });
 
 module.exports = router;
