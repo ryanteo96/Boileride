11bd0b4ea06f5354c05adb73d30eb30cf7d9eead
LCLY <lchoo1996@gmail.com>
2018-09-28 00:15:40 -0400

    refactor: added myRequest and myOffer to myRides route

--- web/routes/myRides.js
+++ web/routes/myRides.js
@@ -6,18 +6,25 @@ const request = require("request");
 router.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/myRides.html"));
 });
+router.get("/myRequest", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/html/myRequest.html"));
+});
+router.get("/myOffer", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/html/myOffer.html"));
+});
 
 router.post("/myRequest", function(req,res){
-	res.sendFile(path.join(__dirname, "../public/html/myRequest.html"));
 	var data = {							
-		userid: req.body.userid
+		// userid: req.body.userid
+		userid:"temp"
 	};
 	var body = {
 		result: 0
 	}
 	res.send(body);
 	console.log("body is " + body);
-	console.log(data);
+	
+	console.log("user id is now " + data);
 
 	//  temp server connection test
 	// var options = {
@@ -32,14 +39,13 @@ router.post("/myRequest", function(req,res){
 
 	// request(options, function(error, response) {
 	// 	console.log(error, response);
-	// 	res.send(response.body);	
+	// 	res.send(response.body);		
 	// 	return;
 	// });
 	
 });
 
-router.post("/myOffer", function(req,res){
-	res.sendFile(path.join(__dirname, "../public/html/myOffer.html"));
+router.post("/myOffer", function(req,res){	
 	var data = {
 		userid: req.body.userid
 	};
