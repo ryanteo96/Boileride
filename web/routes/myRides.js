3f75f8f8d950cc22d856a1c19ae998d6d931cdff
LCLY <lchoo1996@gmail.com>
2018-09-28 22:08:57 -0400

    refactor: added route for myOffer edit and cancel

--- web/routes/myRides.js
+++ web/routes/myRides.js
@@ -19,35 +19,36 @@ router.get("/myRequest/cancel", function(req, res) {
 	res.sendFile(path.join(__dirname, "../public/html/myRequest.html"));
 });
 
+router.get("/myOffer/edit", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/html/myOfferEdit.html"));
+});
+router.get("/myOffer/cancel", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/html/myOffer.html"));
+});
+
 router.post("/myRequest", function(req, res) {
 	var data = {
 		userid: req.body.userid,
-		// userid: "temp",
 	};
-	var body = {
-		result: 0,
-	};
-
-	res.send(body);
-	console.log("user id is now " + data);
 
 	//  temp server connection test
-	// var options = {
-	// 	uri: "http://localhost:8080/ride/view/request",
-	// 	json: data,
-	// 	method: "POST",
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// };
+	var options = {
+		uri: "http://localhost:8080/ride/view/request",
+		json: data,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
 
-	// request(options, function(error, response) {
-	// 	console.log(error, response);
-	// 	res.send(response.body);
-	// 	return;
-	// });
+	request(options, function(error, response) {
+		console.log(error, response);
+		res.send(response.body);
+		return;
+	});
 });
 
+//edit myrequest
 router.post("/myRequest/edit", function(req, res) {
 	let date = req.body.date;
 	let time = moment(req.body.time, "HH:mm").format("HH:mm:ss");
@@ -69,57 +70,8 @@ router.post("/myRequest/edit", function(req, res) {
 	};
 
 	//  temp server connection test
-	// var options = {
-	// 	uri: "http://localhost:8080/ride/update/request",
-	// 	json: data,
-	// 	method: "POST",
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// };
-
-	// request(options, function(error, response) {
-	// 	console.log(error, response);
-	// 	res.send(response.body);
-	// 	return;
-	// });
-});
-
-router.post("/myRequest/edit", function(req, res) {
-	let date = req.body.date;
-	let time = moment(req.body.time, "HH:mm").format("HH:mm:ss");
-
-	var data = {
-		userid: req.body.userid,
-		requestid: req.body.requestid,
-	};
-
-	//  temp server connection test
-	// var options = {
-	// 	uri: "http://localhost:8080/ride/cancel/request",
-	// 	json: data,
-	// 	method: "POST",
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// };
-
-	// request(options, function(error, response) {
-	// 	console.log(error, response);
-	// 	res.send(response.body);
-	// 	return;
-	// });
-});
-
-router.post("/myOffer", function(req, res) {
-	var data = {
-		userid: req.body.userid,
-	};
-	console.log(data);
-
-	//  temp server connection test
 	var options = {
-		uri: "http://localhost:8080/ride/view/offer",
+		uri: "http://localhost:8080/ride/update/request",
 		json: data,
 		method: "POST",
 		headers: {
@@ -134,7 +86,102 @@ router.post("/myOffer", function(req, res) {
 	});
 });
 
-router.post("/myRequest/cancelRide", function(req, res) {
+//cancel myrequest
+router.post("/myRequest/cancel", function(req, res) {
+	let date = req.body.date;
+	let time = moment(req.body.time, "HH:mm").format("HH:mm:ss");
+
+	var data = {
+		userid: req.body.userid,
+		requestid: req.body.requestid,
+	};
+
+	//  temp server connection test
+	var options = {
+		uri: "http://localhost:8080/ride/cancel/request",
+		json: data,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	request(options, function(error, response) {
+		console.log(error, response);
+		res.send(response.body);
+		return;
+	});
+});
+
+//view myOffer
+router.post("/myOffer", function(req, res) {
+	var data = {
+		userid: req.body.userid,
+	};
+	var data = {
+		result: 0,
+	};
+	res.send(data);
+
+	console.log(data);
+
+	//  temp server connection test
+	// var options = {
+	// 	uri: "http://localhost:8080/ride/view/offer",
+	// 	json: data,
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// };
+
+	// request(options, function(error, response) {
+	// 	console.log(error, response);
+	// 	res.send(response.body);
+	// 	return;
+	// });
+});
+
+//edit myOffer
+router.post("/myOffer/edit", function(req, res) {
+	let date = req.body.date;
+	let time = moment(req.body.time, "HH:mm").format("HH:mm:ss");
+
+	var data = {
+		userid: req.body.userid,
+		offerid: req.body.offerid,
+		pickuplocation: req.body.pickuplocation,
+		destination: req.body.destination,
+		datentime: req.body.datentime,
+		seats: req.body.seats,
+		luggage: req.body.luggage,
+		smoking: req.body.smoking,
+		foodndrink: req.body.foodndrink,
+		pets: req.body.pets,
+		ac: req.body.ac,
+		travelingtime: req.body.travelingtime,
+		price: req.body.price,
+	};
+
+	//  temp server connection test
+	var options = {
+		uri: "http://localhost:8080/ride/update/offer",
+		json: data,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	request(options, function(error, response) {
+		console.log(error, response);
+		res.send(response.body);
+		return;
+	});
+});
+
+//cancel myOffer
+router.post("/myOffer/cancel", function(req, res) {
 	var data = {
 		userid: req.body.userid,
 		requestid: req.body.requestid,
@@ -143,7 +190,7 @@ router.post("/myRequest/cancelRide", function(req, res) {
 
 	//  temp server connection test
 	var options = {
-		uri: "http://localhost:8080/ride/cancel/request",
+		uri: "http://localhost:8080/ride/cancel/offer",
 		json: data,
 		method: "POST",
 		headers: {
